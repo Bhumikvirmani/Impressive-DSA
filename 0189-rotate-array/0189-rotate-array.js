19f36124fb5e54058da367d8c93faa81ad2f661f
Bhumik Virmani <virmanibhumik17@gmail.com>
2025-07-06 12:34:57 +0530

    Time: 0 ms (100%), Space: 64.5 MB (71.93%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -4,20 +4,39 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    let n = nums.length;
+    //  Method 1: Using Reverse and Two Pointer 
+    // let n = nums.length;
+    // k %= n; // Get the Point of How much we have to rotate and check if its not get over the size of the array 
+    // reverse(nums,0, n-1); // reverse the whole array 
+    // reverse(nums, 0, k-1); // reverse till the point of k 
+    // reverse(nums, k, n-1);// reverse the other part after k till length of the array
+    // return nums;
+
+    const n = nums.length;
     k %= n;
-    reverse(nums,0, n-1);
-    reverse(nums, 0, k-1);
-    reverse(nums, k, n-1);
-    return nums;
+    let count = 0;
+    //  cyclic replacement algo 
+    for (let start = 0; count < n; start++) {
+        let current = start;
+        let prev = nums[start];
+
+        do {
+            const next = (current + k) % n;
+            const temp = nums[next];
+            nums[next] = prev;
+            prev = temp;
+            current = next;
+            count++;
+        } while (start !== current);
+    }
 };
 
-function reverse(nums, start, end){
-    while(start < end){
-        let temp = nums[end];
-        nums[end] = nums[start];
-        nums[start] = temp;
-        start++;
-        end--;
-    }
-}
+// function reverse(nums, start, end){
+//     while(start < end){
+//         let temp = nums[end];
+//         nums[end] = nums[start];
+//         nums[start] = temp;
+//         start++;
+//         end--;
+//     }
+// }
