cfbcd72d626ca8e4c1bea64aa4a77ee2474a29a0
Bhumik Virmani <virmanibhumik17@gmail.com>
2025-07-06 18:21:12 +0530

    Time: 1 ms (74.42%), Space: 55 MB (21.8%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -3,11 +3,14 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let profit = 0;
-    for(let i = 1; i < prices.length; i++){
-        if(prices[i] > prices[i-1]){
-            profit += prices[i] - prices[i - 1] ;
-        }
-    }
-    return profit;
+    // throgh the reduce function as it reduce the value into final single value 
+    return prices.reduce((acc, curr,idx)=> acc + (curr > prices[idx - 1] ? curr - prices[idx -1]: 0), 0);
+    // brute force without using in built function
+    // let profit = 0;
+    // for(let i = 1; i < prices.length; i++){
+    //     if(prices[i] > prices[i-1]){
+    //         profit += prices[i] - prices[i - 1] ;
+    //     }
+    // }
+    // return prices;
 };
